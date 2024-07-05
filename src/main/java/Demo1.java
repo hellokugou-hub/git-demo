c89ad88f45e9e553cd86db117ee35338f17fad14
hellokugou-hub <chongqishishi@foxmail.com>
2024-07-05 15:38:13 +0800

    Update Demo1.java
    delete one line

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -2,6 +2,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("this is Demo1");
         System.out.println("this is Demo");
-        System.out.println("this is Demo");
+        
     }
 }
